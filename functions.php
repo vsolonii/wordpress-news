9903c7484c695611108e65992a2ec9cea42d18ba
Stas Ponomaryov <ponomaryov.stas@gmail.com>
2018-11-10 23:30:29 +0200

    Initialized function

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
-
+<?php
+/**
+ * Function to display news feed like on news portals
+ * @param int $number - number of news to show
+ * @param null $highlight - ID of a tag. If post has this tag, it's title will be highlighted
+ * @param bool $time - to show or not to show time before title
+ * @param null $exclude - an array that consists of ids of excluding categories
+ */
 function get_articles_feed( $number = 20, $highlight = null, $time = true, $exclude = null ) {
 
 }
